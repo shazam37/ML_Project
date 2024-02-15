e3a7aa2e4da39c47f61fa36067a2022578c5e25c
shazam <shazajmal37@gmail.com>
2024-02-16 03:11:51 +0530

    Hyperparameter training

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ import numpy as np
 import pandas as pd
 import dill
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
 
@@ -20,27 +21,33 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e,sys)
     
-def evaluate_model(X_train,y_train,X_test,y_test,models):
+def evaluate_model(X_train,y_train,X_test,y_test,models,param,cv=3,n_jobs=3):
     try:
         report = {}
+        param_dict = {}
 
         for i in range(len(list(models))):
 
             model=list(models.values())[i]
+            para=param[list(models.keys())[i]]
 
-            model.fit(X_train, y_train)
+            gs=GridSearchCV(model,para,cv=cv,n_jobs=n_jobs)
+            gs.fit(X_train,y_train)
+            
+            param_dict[list(models.keys())[i]] = gs.best_params_
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
 
             y_train_pred=model.predict(X_train)
-
             y_test_pred=model.predict(X_test)
 
             train_model_score=r2_score(y_train,y_train_pred)
-
             test_model_score=r2_score(y_test,y_test_pred)
 
             report[list(models.keys())[i]] = test_model_score
 
-        return report 
+        return (report, param_dict)
     
     except Exception as e:
         raise CustomException(e,sys)
